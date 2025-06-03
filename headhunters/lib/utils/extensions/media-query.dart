6d74d6288248a866a19b0d1ff1bc0a48568ece44
Muhammad Umar <121437794+rajaumarr@users.noreply.github.com>
2025-06-04 01:04:25 +0500

    Delete headhunters directory

--- headhunters/lib/utils/extensions/media-query.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension MediaQueryExtension on BuildContext{
-
-  MediaQueryData get mediaQuery=>MediaQuery.of(this);
-
-  double get screenWidth=>mediaQuery.size.width;
-  double get screenHeight=>mediaQuery.size.height;
-}
--- headhunters/lib/utils/extensions/sizebox.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-extension EmptySpace on num {
-  SizedBox get height => SizedBox (height: toDouble().h);
-  SizedBox get width => SizedBox (width: toDouble().w);
-}
